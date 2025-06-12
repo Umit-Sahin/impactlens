35efced504893e4bf890fd952c757fa99ab0527f
us <umitsahin0@gmail.com>
2025-06-12 16:11:23 +0300

    Add Webpack alias config for Vercel compatibility

--- next.config.js
+++ next.config.js
@@ -1,13 +1,12 @@
-/** @type {import('next').NextConfig} */
-const path = require("path");
+// next.config.js
 
+const path = require('path');
+
+/** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  experimental: {
-    // appDir: true,
-  },
-  webpack(config) {
-    config.resolve.alias["@"] = path.resolve(__dirname, "app");
+  webpack: (config) => {
+    config.resolve.alias['@'] = path.resolve(__dirname, 'app');
+    config.resolve.alias['@lib'] = path.resolve(__dirname, 'lib');
     return config;
   },
 };
