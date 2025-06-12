d46f5ec1cfc7f9b672c18a21d18ff30de171e5c9
us <umitsahin0@gmail.com>
2025-06-12 19:27:54 +0300

    Production deployment fixes

--- lib/stripe.ts
+++ lib/stripe.ts
@@ -1,7 +1,13 @@
 // lib/stripe.ts
 
-
 import Stripe from 'stripe';
 
-export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string, {
+const stripeSecretKey = process.env.STRIPE_SECRET_KEY;
+
+if (!stripeSecretKey) {
+  throw new Error('STRIPE_SECRET_KEY is not defined in environment variables');
+}
+
+export const stripe = new Stripe(stripeSecretKey, {
+  apiVersion: '2023-10-16', // 🔒 Güvenli & kararlı
 });
